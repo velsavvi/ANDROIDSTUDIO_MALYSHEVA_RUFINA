1fbc28d3964213ee3ce557255c1d9cf0d0aee284
velsavvi <jxllolitta@gmail.com>
2023-04-24 00:54:53 +0900

    pr11

---
+++ pr11/src/main/java/ru/startandroid/malysheva/pr11/MainActivity.java
@@ -0,0 +1,54 @@
+package ru.startandroid.malysheva.pr11;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.view.textclassifier.TextLinks;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    TextView view_city;
+    TextView view_temp;
+    TextView view_desc;
+
+    ImageView view_weather;
+    EditText search;
+    FloatingActionButton search_floating;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view_city = findViewById(R.id.town);
+        view_temp = findViewById(R.id.temp);
+        view_desc = findViewById(R.id.desc);
+        view_city.setText("");
+        view_temp.setText("");
+        view_desc.setText("");
+
+        view_weather = findViewById(R.id.weatherimage);
+        search = findViewById(R.id.editText);
+        search_floating = findViewById(R.id.floating_search);
+
+        search_floating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputMethodManager im = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+                im.hideSoftInputFromWindow(getCurrentFocus().getRootView().getWindowToken(),0);
+                api_key(String.valueOf(search.getText()));
+            }
+        });
+    }
+
+    private void api_key(final String City) {
+        OkHttpClient client = new OkHttpClient();
+    }
+}
